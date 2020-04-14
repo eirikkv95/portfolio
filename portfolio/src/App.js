f7d3af2d9574783434a6ca8e4f1db604ffb44ba1
Eirik Kalvø Vattøy <eirik_vattoy@hotmail.com>
2020-04-15 01:19:09 +0200

    React app
    Basic setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/scss/base.scss'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Dette er starten på no nytt min venn</h2>
+    </div>
+  );
+}
+
+export default App;
