e874d44d8ae304e1283a78418dc81c6713417d0e
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-08-20 01:37:59 +0200

    Deletet the blur for now

--- portfolio/src/components/header/header.js
+++ portfolio/src/components/header/header.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import classes from './header.module.scss';
-
+import React from "react";
+import classes from "./header.module.scss";
+import WaveBlur from "../Blur/Blur.js";
 
 const Header = () => {
-
-    return (
-        <section className={classes.header}>
-            <div className={classes.heading}>
-                <h1>ØD</h1>
-            </div>
-            <div className={classes.paragraph}>
-                <p>Heisann! Mitt navn er Eirik og jeg har nylig fullført bachelorgraden min i Webutvikling ved NTNU Gjøvik, og begynner dermed jakten på min første jobb som nyutdannet webutvikler. </p>
-            </div>
-
-        </section>
-
-    );
-}
+  return (
+    <section className={classes.header}>
+      <div className={classes.heading}>
+        <h1>ØD</h1>
+      </div>
+      <div className={classes.paragraph}>
+        <p>
+          Heisann! Mitt navn er Eirik og jeg har nylig fullført bachelorgraden
+          min i Webutvikling ved NTNU Gjøvik, og begynner dermed jakten på min
+          første jobb som nyutdannet webutvikler.{" "}
+        </p>
+      </div>
+      {/* <WaveBlur /> */}
+    </section>
+  );
+};
 
 export default Header;
