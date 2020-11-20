6856a3940f31badde4709b832988aac30456b80f
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-11-20 01:42:21 +0100

    Deleted unused components and deleted all css modu

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './assets/scss/base.scss';
-import classes from './app.module.scss';
 
 import Navigation from './components/navigation/navigation';
 import Header from './components/header/header';
 import About from './components/about/about';
-import ImageGallary from './components/imgGallary/imgGallary';
-import Gif from './components/gif/gif';
 
 function App() {
   return (
     <>
-      <div className={classes.layout}>
-        <Navigation />
-        <Header />
-        <About />
-        <ImageGallary />
-        <Gif />
-      </div>
-      {/* <SocialMedia /> */}
+      <Navigation />
+      <Header />
+      <About />
     </>
   );
 }
