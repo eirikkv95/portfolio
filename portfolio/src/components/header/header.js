d2b24de15f16d3f1646f13ce465e52dc84e17635
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-11-20 01:40:30 +0100

    Gif, image slider and logo deleted

--- portfolio/src/components/header/header.js
+++ portfolio/src/components/header/header.js
@@ -1,23 +1,92 @@
-import React from "react";
-import classes from "./header.module.scss";
-import WaveBlur from "../Blur/Blur.js";
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import Spinn from '../background/spinn';
 
 const Header = () => {
   return (
-    <section className={classes.header}>
-      <div className={classes.heading}>
-        <h1>ØD</h1>
-      </div>
-      <div className={classes.paragraph}>
-        <p>
-          Heisann! Mitt navn er Eirik og jeg har nylig fullført bachelorgraden
-          min i Webutvikling ved NTNU Gjøvik, og begynner dermed jakten på min
-          første jobb som nyutdannet webutvikler.{" "}
-        </p>
-      </div>
-      <WaveBlur />
-    </section>
+    <Wrapper>
+      <Spinn />
+      <ContentWrapper>
+        <TextContent>
+          <Paragraph>
+            Hi! I'm <Span> Eirik Kalvø Vattøy</Span>
+          </Paragraph>
+          <Paragraph>Front End Developer</Paragraph>
+        </TextContent>
+      </ContentWrapper>
+    </Wrapper>
   );
 };
 
 export default Header;
+
+const ShowHeader = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(15rem);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+const ContentWrapper = styled.section`
+  position: relative;
+  overflow: hidden;
+  height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+
+  background: rgba(0, 0, 0, 0.01);
+  backdrop-filter: blur(20px);
+
+  @media (max-width: 640px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 2rem;
+    height: 50vh;
+  }
+  @media (max-width: 410px) {
+  }
+`;
+
+const TextContent = styled.div`
+  max-width: 700px;
+  position: relative;
+  z-index: 11;
+  animation: ${ShowHeader} 1s 0.5s forwards cubic-bezier(0, 0, 0.39, 2);
+  opacity: 0;
+
+  @media (max-width: 630px) {
+    padding: 3rem 0;
+  }
+`;
+
+const Paragraph = styled.p`
+  text-align: center;
+  line-height: 160%;
+  font-size: 4.8rem;
+  color: var(--text);
+
+  @media (max-width: 980px) {
+    font-size: 4.2rem;
+  }
+
+  @media (max-width: 640px) {
+    font-size: 3rem;
+  }
+
+  @media (max-width: 440px) {
+    font-size: 2rem;
+  }
+`;
+
+const Span = styled.span`
+  color: var(--primary);
+`;
