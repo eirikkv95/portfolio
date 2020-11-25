7e14ccec4495b0aefb93e575d2e3f733e61bdbe0
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-11-25 03:03:31 +0100

    lazy loading, suspense, text now english

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styled, { keyframes } from 'styled-components';
-
-import Sparkles from '../sparkle/sparkles';
-import Eirik from '../../assets/images/eirikNew.jpg';
+import { BodyText, H2 } from '../styles/textstyles';
+import Eirik from '../../assets/images/eirikNew.webp';
 import Rings from '../../assets/svg/ring.svg';
+import Loader from '../Loader/loader.js';
+const Sparkles = React.lazy(() => import('../sparkle/sparkles'));
 
 const About = () => {
   return (
     <Wrapper>
       <ContentWrapper>
-        <ImageWrapper>
-          <Img src={Eirik} alt="Eirik Kalvø Vattøy" />
-          <Ring src={Rings} alt="ring" />
-        </ImageWrapper>
-        <Info>
-          <HeadingTwo>Hvem er jeg</HeadingTwo>
-          <Paragraph>
-            Jeg er en 25 år gammel mann fra Ulsteinvik som er spesielt
-            interessert i <Sparkles>UI</Sparkles>. Jeg har en Bachelor i Web
-            Utvikling og er for øyeblikket på jakt etter min første jobb som
-            nyutdannet webutvikler.
-          </Paragraph>
+        <Suspense fallback={<Loader />}>
+          <ImageWrapper>
+            <Img src={Eirik} alt="Eirik Kalvø Vattøy" />
+            <Ring src={Rings} alt="ring" />
+          </ImageWrapper>
+          <Info>
+            <HeadingTwo>Who am I</HeadingTwo>
+            <Paragraph>
+              Hi, I'm Eirik, a friendly, 20 something-year-old Front End
+              Developer with an educational background in{' '}
+              <Sparkles>Web development</Sparkles>.
+            </Paragraph>
 
-          <Paragraph>
-            Jeg vil anse meg selv som en lojal, avslappet og samarbeidsvillig
-            person som liker en utfordring. Selv om jeg kan være litt sjenert i
-            starten, blir jeg fort varm i trøya.
-          </Paragraph>
-        </Info>
+            <Paragraph>
+              During my studies and knowledge gained in my spare time, I have
+              worked up experience with technologies such as Html, Css/Sass,
+              JavaScript, React and Github. I'm also interested in design and
+              have experience working with responsiv design from different
+              projects.
+            </Paragraph>
+          </Info>
+        </Suspense>
       </ContentWrapper>
     </Wrapper>
   );
@@ -37,13 +41,11 @@ export default About;
 
 const Wrapper = styled.div`
   position: relative;
-
-  @media (max-width: 640px) {
-    padding: 0 2rem;
-  }
 `;
 const ImageWrapper = styled.div`
   display: grid;
+
+  align-items: center;
   position: relative;
   @media (max-width: 680px) {
     display: none;
@@ -51,6 +53,7 @@ const ImageWrapper = styled.div`
 `;
 const Img = styled.img`
   width: 28rem;
+  height: 28rem;
   border-radius: 50%;
   &:after {
     content: '';
@@ -73,9 +76,22 @@ const Ring = styled.img`
   position: absolute;
   width: 31rem;
   height: 31rem;
-  top: -16px;
+  top: 9px;
   left: -15px;
   animation: ${spin} 80s infinite linear;
+
+  @media (max-width: 1020px) {
+    top: -8px;
+  }
+  @media (max-width: 831px) {
+    top: 5px;
+  }
+  @media (max-width: 751px) {
+    top: 16px;
+  }
+  @media (max-width: 689px) {
+    top: 30px;
+  }
 `;
 
 const ContentWrapper = styled.div`
@@ -88,9 +104,10 @@ const ContentWrapper = styled.div`
   gap: 2rem;
 
   @media (max-width: 680px) {
+    max-width: 500px;
     display: block;
     margin: 5rem auto;
-    padding: 0;
+    padding: 0 2rem;
   }
 `;
 
@@ -100,8 +117,7 @@ const Info = styled.div`
   top: 7.5rem;
   display: grid;
   gap: 2rem;
-  @media (max-width: 780px) {
-  }
+
   @media (max-width: 640px) {
     grid-column: 1/-1;
     grid-row: 2/3;
@@ -109,32 +125,24 @@ const Info = styled.div`
   }
 `;
 
-const HeadingTwo = styled.h2`
-  font-size: 4rem;
-  font-weight: 500;
+const HeadingTwo = styled(H2)`
   color: var(--primary);
   position: relative;
 
-  @media (max-width: 1020px) {
-    font-size: 3rem;
-  }
-
-  @media (max-width: 1020px) {
-    width: 25rem;
-  }
-  @media (max-width: 640px) {
+  @media (max-width: 680px) {
     content: none;
+    font-size: 4rem;
   }
 `;
 
-const Paragraph = styled.p`
-  font-size: 1.9rem;
-  font-weight: 500;
-  line-height: 150%;
+const Paragraph = styled(BodyText)`
   color: var(--text);
   position: relative;
 
   @media (max-width: 1020px) {
     font-size: 1.6rem;
+  }
+  @media (max-width: 480px) {
+    font-size: 1.4rem;
   }
 `;
