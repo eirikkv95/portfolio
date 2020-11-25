018e21684f4ed0dde4a81850f326c751c7cba857
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-11-25 03:08:52 +0100

    Added a textstyles file so I don't repeat myself

---
+++ portfolio/src/components/styles/textstyles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  font-size: 6.4rem;
+  font-weight: 600;
+`;
+
+export const H2 = styled.h2`
+  font-size: 5.4rem;
+  font-weight: 500;
+`;
+
+export const H3 = styled.h3`
+  font-size: 4rem;
+  font-weight: 500;
+`;
+
+export const BodyText = styled.p`
+  font-size: 1.9rem;
+  font-weight: 300;
+  line-height: 150%;
+`;
